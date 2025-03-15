cb9738b7722a1290c17d4984f84a510137426ab4
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-03-16 04:50:19 +0800

    Remove duplicate route definition for temporary storage in web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
     
-    Route::post('temp/storage', [\App\Http\Controllers\StorageController::class, 'store'])->name('storage.store');
     Route::delete('/settings/profile/delete-file', [\App\Http\Controllers\Settings\ProfileController::class, 'deleteFile'])->name('profile.deleteFile');
     Route::post('/settings/profile/upload', [\App\Http\Controllers\Settings\ProfileController::class, 'upload'])->name('profile.upload');
     Route::post('/temp/storage', [\App\Http\Controllers\StorageController::class, 'store'])->name('storage.store');
