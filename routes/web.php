296f1f33e4729458b73b361f32b1c6b0f6cb07be
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-03-18 00:37:23 +0800

    Add JSON endpoints for permissions and users in web routes

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
-    
+
     Route::delete('/settings/profile/delete-file', [\App\Http\Controllers\Settings\ProfileController::class, 'deleteFile'])->name('profile.deleteFile');
     Route::post('/settings/profile/upload', [\App\Http\Controllers\Settings\ProfileController::class, 'upload'])->name('profile.upload');
     Route::post('/temp/storage', [\App\Http\Controllers\StorageController::class, 'store'])->name('storage.store');
@@ -26,9 +26,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::post('roles/json', [\App\Http\Controllers\UserRolePermission\RoleController::class, 'json'])->name('roles.json');
     Route::resource('roles', \App\Http\Controllers\UserRolePermission\RoleController::class);
-   
+
+    Route::post('permissions/json', [\App\Http\Controllers\UserRolePermission\PermissionController::class, 'json'])->name('permissions.json');
     Route::resource('permissions', \App\Http\Controllers\UserRolePermission\PermissionController::class);
-    
+
+    Route::post('users/json', [\App\Http\Controllers\UserRolePermission\UserController::class, 'json'])->name('users.json');
     Route::resource('users', \App\Http\Controllers\UserRolePermission\UserController::class);
     Route::get('users/trashed', [\App\Http\Controllers\UserRolePermission\UserController::class, 'trashed'])->name('users.trashed');
     Route::get('users/{user}/restore', [\App\Http\Controllers\UserRolePermission\UserController::class, 'restore'])->name('users.restore');
@@ -39,7 +41,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
-
 
 // "datatables.net": "^2.2.2",
 // "datatables.net-dt": "^2.2.2",
