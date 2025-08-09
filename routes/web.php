c9292f18d4fa9dd14dd411e0b9cb07d90dfe0a13
RahmatRafiq <rahmatrafiq.1999@.com>
2025-08-09 21:04:26 +0800

    refactor: add folder management routes in GalleryController for create, rename, and delete functionalities

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::post('gallery', [\App\Http\Controllers\GalleryController::class, 'store'])->name('gallery.store');
         Route::delete('gallery/{id}', [\App\Http\Controllers\GalleryController::class, 'destroy'])->name('gallery.destroy');
         Route::get('gallery/file/{id}', [\App\Http\Controllers\GalleryController::class, 'file'])->name('gallery.file');
+        Route::post('gallery/folder', [\App\Http\Controllers\GalleryController::class, 'createFolder'])->name('gallery.folder.create');
+        Route::put('gallery/folder/{id}', [\App\Http\Controllers\GalleryController::class, 'renameFolder'])->name('gallery.folder.rename');
+        Route::delete('gallery/folder/{id}', [\App\Http\Controllers\GalleryController::class, 'deleteFolder'])->name('gallery.folder.delete');
     });
 
     Route::middleware('role:admin')->prefix('admin')->name('admin.')->group(function () {
