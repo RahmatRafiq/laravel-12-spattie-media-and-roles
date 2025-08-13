55d925f5228e3baa9dbc69d7114d7578efcdd709
RahmatRafiq <rahmatrafiq.1999@.com>
2025-08-13 18:44:27 +0800

    feat: implement Permission model factory and add comprehensive tests for permission management

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Permission extends \Spatie\Permission\Models\Permission
 {
+    use HasFactory;
+
     protected $casts = [
         'created_at' => 'datetime:Y-m-d H:i:s',
         'updated_at' => 'datetime:Y-m-d H:i:s',
